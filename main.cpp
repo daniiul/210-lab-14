397336d4c13031c850cd379997e7c5e22d06cdef
daniiul <dmalakhov2005@gmail.com>
2024-09-27 18:14:57 -0700

    Created class and added get/setter functions

--- main.cpp
+++ main.cpp
@@ -2,7 +2,21 @@
 #include <iomanip>
 using namespace std;
 
+class Color{
+    private:
+        int red;
+        int green;
+        int blue;
+    public:
+        double getRed() { return red;}
+        void setRed(int r) { red = r;}
+        double getGreen() { return green;}
+        void setGreen(int g) { green = g;}
+        double getBlue() { return blue;}
+        void setRed(int b) { blue = b;}
+};
+
 int main() 
 {
-    
+
 }
